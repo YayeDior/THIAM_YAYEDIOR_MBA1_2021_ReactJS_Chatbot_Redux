ff6418cfac4c58107c92001c3a95691c015850d0
YayeDior <diordogo97@gmail.com>
2021-03-22 00:40:57 +0000

    commit projet

---
+++ src/components/conversation.js
@@ -0,0 +1,13 @@
+var watson = watson.conversation
+
+var conversation = watson.conversation({
+        "text": "Bonjour,que puis-je faire pour vous?",
+        "entity": "BOT",
+        "text": "Je cherche une solution",
+        "entity": "User",
+        "text": "Je vous écoute",
+        "entity": "BOT",
+        "text": "Merci",
+        "entity": "User"
+});
+
